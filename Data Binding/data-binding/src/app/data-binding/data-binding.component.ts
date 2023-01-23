5d49180729f43689a4ed1a253c97e45f32f17f43
vinicioslop <vinicioslop7@hotmail.com>
2023-01-23 12:53:46 -0300

    Finaliza aula de Event Binding

--- Data Binding/data-binding/src/app/data-binding/data-binding.component.ts
+++ Data Binding/data-binding/src/app/data-binding/data-binding.component.ts
@@ -10,12 +10,33 @@ export class DataBindingComponent implements OnInit {
   cursoAngular: boolean = true;
   urlImagem = 'http://lorempixel.com.br/400/200';
 
+  valorAtual: string = '';
+  valorSalvo: string = '';
+
+  isMouseOver: boolean = false;
+
   getValor() {
     return 1;
   }
 
   getCurtirCurso() {
     return true;
+  }
+
+  botaoClicado() {
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor: string) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() {
